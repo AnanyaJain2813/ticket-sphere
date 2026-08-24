82188e822da87daa68f2c96b83af30673dda1036
AnanyaJain2813 <AnanyaJain2813@users.noreply.github.com>
2026-08-24 18:04:56 +0530

    chore: remove unwanted files and clean up repository

--- get_shows.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, json
-shows = json.loads(urllib.request.urlopen("https://ticketsphere-api-ypje.onrender.com/api/shows/").read())
-for s in shows:
-    if "ICON IMAX" in s["venue_name"]:
-        print(s["id"])
